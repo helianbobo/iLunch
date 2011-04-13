5833528734d561ec9be55eb11083680fa07c446e
liuchao <liuchao36@gmail.com>
2011-04-13 14:39:58 +0800

    functional test spec example

---
+++ code/trunk/prototype/src/groovy/cn/ilunch/test/HttpFunctionalSpec.groovy
@@ -0,0 +1,12 @@
+package cn.ilunch.test
+
+import spock.lang.Specification
+
+
+class HttpFunctionalSpec extends Specification{
+
+    protected String get(String url){
+        return null
+    }
+
+}
--- code/trunk/prototype/test/functional/MySpec.groovy
+++ code/trunk/prototype/test/functional/MySpec.groovy
@@ -1,15 +1,18 @@
-import spock.lang.Specification
-import grails.plugin.spock.UnitSpec
+import cn.ilunch.test.HttpFunctionalSpec
 
-class MySpec extends UnitSpec{
+class MySpec extends HttpFunctionalSpec{
 
     def "主菜列表"(){
 
         when: "页面读取完毕,向后台发送ajax数据请求"
 
+        def response = get(url)
 
         then: "后台返回json格式的数据"
+        response == '[{name:"咖喱鱼"}]'
 
+        where:
+        url = 'http://localhost:8080/prototype/mainDish/show'
 
     }
 
