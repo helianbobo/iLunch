4e9ca39c705bbddacf35f6b554534b1f70596fa9
JLeo <ggyyleo@gmail.com>
2011-05-05 18:58:11 -0700

    add cart not found

--- code/trunk/prototype/grails-app/controllers/cn/ilunch/domain/ExceptionController.groovy
+++ code/trunk/prototype/grails-app/controllers/cn/ilunch/domain/ExceptionController.groovy
@@ -13,4 +13,9 @@ class ExceptionController {
             error(message: "Price schedule not found of product ${params.product} in area ${params.area} on date ${params.date}", errorCode: grailsApplication.config.cn.ilunch.exception.code.ScheduleNotFound.toString())
         }
     }
+def cartNotFound = {
+        render(contentType: "text/json") {
+            error(message: "cart not found", errorCode: grailsApplication.config.cn.ilunch.exception.code.CartNotFound.toString())
+        }
+    }
 }
