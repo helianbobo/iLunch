ca74ab9ac3d02d388cfeef117dcc14ec6df49a2e
JLeo <ggyyleo@gmail.com>
2011-05-17 07:41:39 -0700

    add check for senario not enough point change

--- code/trunk/prototype/grails-app/controllers/cn/ilunch/domain/ProductOrderController.groovy
+++ code/trunk/prototype/grails-app/controllers/cn/ilunch/domain/ProductOrderController.groovy
@@ -1,7 +1,5 @@
 package cn.ilunch.domain
 
-import cn.ilunch.exception.EntityNotFoundException
-
 
 class ProductOrderController {
 
@@ -9,24 +7,31 @@ class ProductOrderController {
 
     def confirm = {
         def orderDetailsJSON = request.getJSON()
-        def orderDetails = orderDetailsJSON
+        def orderDetails = grails.converters.JSON.parse(orderDetailsJSON)
 
         def userId = orderDetails.id
         def buildingId = orderDetails.buildingId
-		
+
         def customer = Customer.get(userId)
         def building = Building.get(buildingId)
-		
+
         if (!customer) {
             forward(controller: "exception", action: "entityNotFound", params: [id: userId, entityName: Customer])
             return;
         }
-		
+
         if (!building) {
             forward(controller: "exception", action: "entityNotFound", params: [id: buildingId, entityName: Building])
             return;
         }
+        def pointChangePoint = orderDetails.pointChange as float
+        if (pointChangePoint > (customer.pointBalance as float)) {
+            forward(controller: "exception", action: "notEnoughPointChange", params: [id: userId])
+            return;
+        }
+
         def dateFormatString = grailsApplication.config.cn.ilunch.date.format
+
         try {
             def productOrder = orderService.createOrder(orderDetails, customer, building, dateFormatString)
             render(contentType: "text/json") {
